8aa51fb87d3e2f43f01dab81ecf11545f1ddf2f4
Kyong <kyongbest@163.com>
2024-09-19 09:53:52 +0800

    finish:push unshift parameter

--- playground/easy/03057-easy-push.ts
+++ playground/easy/03057-easy-push.ts
@@ -18,16 +18,16 @@
 
 /* _____________ 你的代码 _____________ */
 
-type Push<T, U> = any
+type Push1<T extends any[], U> = [...T, U];
 
 /* _____________ 测试用例 _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
 
 type cases = [
-  Expect<Equal<Push<[], 1>, [1]>>,
-  Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
-  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>,
-]
+    Expect<Equal<Push1<[], 1>, [1]>>,
+    Expect<Equal<Push1<[1, 2], '3'>, [1, 2, '3']>>,
+    Expect<Equal<Push1<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>
+];
 
 /* _____________ 下一步 _____________ */
 /*
--- playground/easy/03060-easy-unshift.ts
+++ playground/easy/03060-easy-unshift.ts
@@ -18,16 +18,16 @@
 
 /* _____________ 你的代码 _____________ */
 
-type Unshift<T, U> = any
+type Unshift1<T extends any[], U> = [U, ...T];
 
 /* _____________ 测试用例 _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from '@type-challenges/utils';
 
 type cases = [
-  Expect<Equal<Unshift<[], 1>, [1]>>,
-  Expect<Equal<Unshift<[1, 2], 0>, [0, 1, 2]>>,
-  Expect<Equal<Unshift<['1', 2, '3'], boolean>, [boolean, '1', 2, '3']>>,
-]
+    Expect<Equal<Unshift1<[], 1>, [1]>>,
+    Expect<Equal<Unshift1<[1, 2], 0>, [0, 1, 2]>>,
+    Expect<Equal<Unshift1<['1', 2, '3'], boolean>, [boolean, '1', 2, '3']>>
+];
 
 /* _____________ 下一步 _____________ */
 /*
