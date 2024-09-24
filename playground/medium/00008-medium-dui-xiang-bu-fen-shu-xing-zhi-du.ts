a8a808a33b0cf0bb2753a79252560c33243201f3
Kyong <kyongbest@163.com>
2024-09-24 14:13:10 +0800

    finish: MyReadonly2

--- playground/medium/00008-medium-dui-xiang-bu-fen-shu-xing-zhi-du.ts
+++ playground/medium/00008-medium-dui-xiang-bu-fen-shu-xing-zhi-du.ts
@@ -33,38 +33,42 @@
 */
 
 /* _____________ 你的代码 _____________ */
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+    readonly [k in K]: T[k];
+} & {
+    [P in keyof T as P extends K ? never : P]: T[P];
+};
 
-type MyReadonly2<T, K> = any
 
 /* _____________ 测试用例 _____________ */
-import type { Alike, Expect } from '@type-challenges/utils'
+import type { Alike, Expect } from '@type-challenges/utils';
 
 type cases = [
-  Expect<Alike<MyReadonly2<Todo1>, Readonly<Todo1>>>,
-  Expect<Alike<MyReadonly2<Todo1, 'title' | 'description'>, Expected>>,
-  Expect<Alike<MyReadonly2<Todo2, 'title' | 'description'>, Expected>>,
-  Expect<Alike<MyReadonly2<Todo2, 'description' >, Expected>>,
-]
+    Expect<Alike<MyReadonly2<Todo1>, Readonly<Todo1>>>,
+    Expect<Alike<MyReadonly2<Todo1, 'title' | 'description'>, Expected>>,
+    Expect<Alike<MyReadonly2<Todo2, 'title' | 'description'>, Expected>>,
+    Expect<Alike<MyReadonly2<Todo2, 'description'>, Expected>>
+];
 
 // @ts-expect-error
-type error = MyReadonly2<Todo1, 'title' | 'invalid'>
+type error = MyReadonly2<Todo1, 'title' | 'invalid'>;
 
 interface Todo1 {
-  title: string
-  description?: string
-  completed: boolean
+    title: string;
+    description?: string;
+    completed: boolean;
 }
 
 interface Todo2 {
-  readonly title: string
-  description?: string
-  completed: boolean
+    readonly title: string;
+    description?: string;
+    completed: boolean;
 }
 
 interface Expected {
-  readonly title: string
-  readonly description?: string
-  completed: boolean
+    readonly title: string;
+    readonly description?: string;
+    completed: boolean;
 }
 
 /* _____________ 下一步 _____________ */
